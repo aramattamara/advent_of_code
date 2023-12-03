1bc30fa595bca210735685d4ebb0d82baaa77684
Tamara Lazerka <lazerkatamara@gmail.com>
2023-12-02 21:13:25 -0700

    Day 1: Trebuchet https://adventofcode.com/2023/day/1

---
+++ day_1_trebuchet/part_1.js
@@ -0,0 +1,34 @@
+
+const fs = require("fs");
+
+fs.readFile("in.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        // let resglobal = 0;
+        // .map.forEach.filter.combine
+
+        // const resglobal= data
+        //     .trimEnd()
+        //     .split("\n")
+        //     .map((line) => {
+        //         line.replaceAll(/\D/g, '')
+        //     })
+        //     .map((numb) => {
+        //         String(numb[0] + numb[numb.length - 1])
+        //     })
+        //     .reduce((res, str) => {
+        //         res + parseInt(str)
+        //     }, 0)
+
+        let resglobal = 0;
+        const lines = data.trimEnd().split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            let numb = lines[i].match(/\d/g);
+            res = numb[0] + numb[numb.length - 1];
+            resglobal += parseInt(res);
+            console.log(`lines[i]=${lines[i]}, numb=${numb}, res=${res}, resglobal=${resglobal}`);
+        }
+        console.log(resglobal);
+    }
+});
