b65db4387ac2b643063a1e00688d4e6e8617db8f
Tamara Lazerka <lazerkatamara@gmail.com>
2023-12-03 23:47:06 -0700

    Solving Day 3

---
+++ README.md
@@ -0,0 +1,1 @@
+This folder was created for https://adventofcode.com/2023
---
+++ day_3_gear_ratios/part_1.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+fs.readFile("in.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        const rows = data.trim().split('\n');
+        let sum = 0;
+
+        const N = rows.length;
+        const M = rows[0].length;
+
+        const allNumbers = [1];
+        allNumbers.pop();
+
+        for (let i = 0; i < N; i++) {
+            let curNum = -1;
+
+            for (let j = 0; j < M; j++) {
+                const c = rows[i][j];
+
+                if (/\d/.test(c)) {
+                    // Parse current number
+                    if (curNum === -1) {
+                        curNum = parseInt(c);
+                    } else {
+                        curNum *= 10;
+                        curNum += parseInt(c);
+                    }
+                } else if (curNum !== -1) {
+                    allNumbers.push(curNum);
+                    curNum = -1;
+                }
+            }
+        }
+
+        console.log(allNumbers);
+    }
+});
