b4fa89d3a7b8133891c139f7e2f492f5f96e19cb
Tamara Lazerka <lazerkatamara@gmail.com>
2023-12-04 20:13:23 -0800

    Solving Day 3, test is true, real case is failed

--- day_3_gear_ratios/part_1.js
+++ day_3_gear_ratios/part_1.js
@@ -1,56 +1,90 @@
 const fs = require("fs");
 
-fs.readFile("in.txt", "utf-8", (err, data) => {
+fs.readFile("input.txt", "utf-8", (err, data) => {
+
     if (err) {
         console.log(err);
-    } else {
-        const rows = data.trim().split('\n');
-        let sum = 0;
-
-        const N = rows.length;
-        const M = rows[0].length;
-
-        const whichNumber = [];
-        // Initialize whichNumber
-        for (let i = 0; i < N; i++) {
-            whichNumber.push([]);
-            for (let j = 0; j < M; j++) {
-                whichNumber[whichNumber.length - 1].push(-1);
-            }
-        }
-
-        const allNumbers = [1];
-        allNumbers.pop();
-
-        for (let i = 0; i < N; i++) {
-            let curNum = -1;
-
-            for (let j = 0; j < M; j++) {
-                const c = rows[i][j];
-
-                if (/\d/.test(c)) {
-                    // Parse current number
-                    if (curNum === -1) {
-                        curNum = parseInt(c);
-                    } else {
-                        curNum *= 10;
-                        curNum += parseInt(c);
-                    }
-                    whichNumber[i][j] = allNumbers.length;
-                } else if (curNum !== -1) {
-                    allNumbers.push(curNum);
-                    curNum = -1;
-                }
-            }
-        }
-
-        const seen = [];
-        for (let i = 0; i < allNumbers.length; i++) {
-            seen.push(false);
-        }
-
-        console.log(allNumbers);
-        console.log(seen);
-        console.log(whichNumber);
+        return;
     }
+
+
+    const rows = data.trim().split('\n');
+    let sum = 0;
+
+    const N = rows.length;
+    const M = rows[0].length;
+
+    const whichNumber = [];
+    // Initialize whichNumber
+    for (let i = 0; i < N; i++) {
+        whichNumber.push([]);
+        for (let j = 0; j < M; j++) {
+            whichNumber[whichNumber.length - 1].push(-1);
+        }
+    }
+
+    const allNumbers = [1];
+    allNumbers.pop();
+
+
+
+    for (let i = 0; i < N; i++) {
+        let curNum = -1;
+
+        for (let j = 0; j < M; j++) {
+            const c = rows[i][j];
+
+            if (/\d/.test(c)) {
+                // Parse current number
+                if (curNum === -1) {
+                    curNum = parseInt(c);
+                } else {
+                    curNum *= 10;
+                    curNum += parseInt(c);
+                }
+                whichNumber[i][j] = allNumbers.length;
+            } else if (curNum !== -1) {
+                allNumbers.push(curNum);
+                curNum = -1;
+            }
+        }
+    }
+
+    const seen = [];
+    for (let i = 0; i < allNumbers.length; i++) {
+        seen.push(false);
+    }
+
+    function check(i, j) {
+        let k = whichNumber[i][j];
+        if (k !== -1) {
+            if (seen[k] === false) {
+                sum += allNumbers[k];
+                seen[k] = true;
+            }
+        }
+    }
+
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++){
+            const c = rows[i][j];
+
+            if (!/[\d.]/.test(c)) {
+                check(i - 1, j - 1);
+                check(i - 1, j);
+                check(i - 1, j + 1);
+                check(i, j - 1);
+                check(i, j + 1);
+                check(i + 1, j - 1);
+                check(i + 1, j);
+                check(i + 1, j + 1);
+            }
+        }
+    }
+
+    console.log(allNumbers);
+    console.log(seen);
+    console.log(whichNumber);
+    console.log(sum);
+
 });
